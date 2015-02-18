14f12410ffae4a6b908b13ba6918c5d02b728039
Amy Johnson and Joey Sandine <jasandine@gmail.com>
2015-02-18 14:50:41 -0800

    Created function to check a release date on movie and apply discount or NOT

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 var Ticket ={
   price: 15,
-  age: function(ageCheck){
-    if (ageCheck === "child"){
+  age: function(ageCheck) {
+    if (ageCheck === "child") {
       this.price -= 7;
     } else if (ageCheck === "senior") {
       this.price -= 8;
     }
+  },
+  time: function(timeCheck) {
+    if (timeCheck === "matinee") {
+      this.price -= 5;
+    }
+  },
+  release: function(releaseCheck) {
+    if (releaseCheck < 2013 ) {
+      this.price -= 8;
+    }
   }
-
 };
--- spec/specs.js
+++ spec/specs.js
@@ -18,7 +18,19 @@ describe("Ticket", function() {
       testTicket.age("senior");
       expect(testTicket.price).to.equal(7);
     });
+  });
 
+  describe("time", function(){
+    it("sets a price for evening show", function(){
+      var testTicket = Object.create(Ticket);
+      testTicket.time("evening");
+      expect(testTicket.price).to.equal(15);
+    });
+    it("sets a price for a matinee", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.time("matinee");
+      expect(testTicket.price).to.equal(10);      
+    });
   });
 
 
